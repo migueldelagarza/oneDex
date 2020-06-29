8da6e8846b9a6814c6b84b1387e849d4337d12ff
Miguel de la Garza <miguel.delagarza@banregio.com>
2020-06-29 16:51:17 -0500

    Se modifica ubicación de componentes

---
+++ src/app/core/components/footer.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { SearchPokemonService } from '../services/search-pokemon.service';
+import { PageData } from '@constants/pages';
+import { PageContent } from '@models/page';
+
+@Component({
+  selector: 'one-footer',
+  template: `
+    <footer mat-tab-nav-bar mat-align-tabs="center" backgroundColor="default">
+      <a mat-tab-link *ngFor="let link of links" [routerLink]="link.route"
+        routerLinkActive #linkActive="routerLinkActive"
+        [active]="linkActive.isActive">
+        <mat-icon>{{link.icon}}</mat-icon>
+        <small>{{link.name}}</small>
+      </a>
+    </footer>
+  `,
+  styles: [`
+    footer {
+      bottom: 0;
+      position: fixed;
+      width: 100%;
+    }
+  `]
+})
+export class FooterComponent {
+  links: PageContent[];
+
+  constructor(private search: SearchPokemonService) {
+    this.links = Object.values(PageData);
+  }
+
+  public openSearch(): void {
+    this.search.openSearch();
+  }
+
+}
---
+++ src/app/core/components/header.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'one-header',
+  template: `
+    <mat-toolbar>
+      <span>
+        {{ title }}
+      </span>
+      <button mat-icon-button class="mat-elevation-z0">
+        <mat-icon>more_vert</mat-icon>
+        <span class="mat-small"></span>
+      </button>
+    </mat-toolbar>
+  `,
+  styles: [`
+    mat-toolbar {
+      background: transparent
+    }
+  `]
+})
+export class HeaderComponent {
+  @Input() drawer: MatDrawer;
+  title: string;
+
+  constructor() {
+    this.title = 'OneDex';
+  }
+
+}
