9015eca9f83ee23d9b2a4c786278e514e9097b4d
Miguel de la Garza <htmike.codr@gmail.com>
2022-08-17 15:36:13 -0500

    Se implementa localstorage

--- src/app/shared/services/recents.service.ts
+++ src/app/shared/services/recents.service.ts
@@ -1,22 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class RecentsService {
   recents: any[];
 
   constructor() {
-    this.recents = [];
+    const localData = JSON.parse(localStorage.getItem("recents"));
+    this.recents = localData || [];
   }
 
-  public addRecentPokemon( pokemon: any, specie: any ) {
-    const index = this.recents.findIndex( data => {
+  public addRecentPokemon(pokemon: any, specie: any) {
+    const index = this.recents.findIndex((data) => {
       return data.pokemon.id === pokemon.id;
     });
     if (index >= 0) {
       this.recents.splice(index, 1);
     }
-    this.recents.unshift({pokemon, specie});
+    this.recents.unshift({ pokemon, specie });
+    this.saveLocal();
+  }
+
+  private saveLocal(): void {
+    const pokemon = JSON.stringify(this.recents);
+    localStorage.setItem("recents", pokemon);
   }
 }
